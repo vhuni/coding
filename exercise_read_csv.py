a8014dc1a36704a4a82ac478d466b2a68e532fc1
Boney Labinghisa <blabinghisa@yahoo.com>
2023-02-24 13:57:04 +0000

    deleted class exception and added raise exception

--- exercise_read_csv.py
+++ exercise_read_csv.py
@@ -1,23 +1,22 @@
 import csv
 
-class StudentFailException:
-    print('Student failed')
-
 
 with open('student_grades.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        if line_count == 0:
-            print(f'Columns names are {", ".join(row)}')
-            line_count += 1
-        else:
-            average = (int(row[1]) + int(row[2]) + int(row[3])) / 3
-            print(f'{row[0]}\'s average score is {average}')
-            if average < 70:
-                raise StudentFailException
-            line_count += 1
-
+        try:
+            if line_count == 0:
+                print(f'Columns names are {", ".join(row)}')
+                line_count += 1
+            else:
+                average = (int(row[1]) + int(row[2]) + int(row[3])) / 3
+                print(f'{row[0]}\'s average score is {average}')
+                line_count += 1
+                if average < 70:
+                    raise Exception
+        except Exception:
+            print(f'{row[0]} failed')
 
 print(f'Processed {line_count} lines.')
 
