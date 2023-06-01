4bf488ec186fc33e973b5d948b50c28c08163a5b
vhuni <blabinghisa@yahoo.com>
2023-06-01 20:05:59 +0100

    added simple code

--- codewars_list_filter.py
+++ codewars_list_filter.py
@@ -1,9 +1,11 @@
 def filter_list(l):
     'return a new list with the strings filtered out'
-    new_list = []
-    for item in l:
-        if (type(item) == type(1)):
-            new_list.append(item)
-    return new_list
+    # new_list = []
+    # for item in l:
+    #     if (type(item) == type(1)):
+    #         new_list.append(item)
+    # return [i for i in l if not isinstance(i, str)]
+    return [i for i in l if type(i) is not str]
+    # return new_list
 
 print(filter_list([1,2,'aasf','1','123',123]))
